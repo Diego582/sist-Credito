fd4700ee76791ceed5b2dad75e49e57b0b2c5d89
Diego582 <penaomardiego@gmail.com>
2022-03-08 15:22:05 -0300

    migration tipos clientes_creditos

---
+++ src/database/migrations/2022_03_08_175127_create_clientes_creditos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Beneficiario;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes_creditos', function (Blueprint $table) {
+            $table->unsignedBigInteger('credito_id');
+            $table->unsignedBigInteger('beneficiario_id');
+            $table->unsignedBigInteger('tipo_id');
+            $table->primary(['credito_id', 'tipo_id']);
+            $table->foreign('credito_id')->references('id')->on('creditos');
+            $table->foreign('beneficiario_id')->references('id')->on('beneficiarios');
+            $table->foreign('tipo_id')->references('id')->on('tipos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes_creditos');
+    }
+};
